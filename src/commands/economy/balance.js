f12e4ba67477d6b2ed8094d06d20f88ef6100111
joanferrecid098 <joanferrecid@gmail.com>
2023-12-22 22:58:33 +0100

    Mentionable /balance Command

--- src/commands/economy/balance.js
+++ src/commands/economy/balance.js
@@ -1,29 +1,39 @@
 const { SlashCommandBuilder } = require("discord.js");
-const UserProfile = require('../../schemas/UserProfile.js');
+const { ApplicationCommandOptionType } = require('discord.js');
+const UserProfile = require('../../schemas/UserProfile');
 
 module.exports = {
-  data: new SlashCommandBuilder().setName('balance').setDescription('Check your current balance.'),
+  data: new SlashCommandBuilder()
+    .setName('balance')
+    .setDescription('Check your current balance.')
+    .addUserOption((option) =>
+      option
+        .setName('target-user')
+        .setDescription('The user whose balance you want to see.')
+    ),
+    
   run: async ({ interaction }) => {
-    try {
-      await interaction.deferReply({
+    if (!interaction.inGuild()) {
+      interaction.reply({
+        content: 'This command can only be executed inside a server.',
         ephemeral: true,
       });
-      
-      let userProfile = await UserProfile.findOne({
-        userId: interaction.member.id,
-      });
+      return;
+    }
+
+    const targetUserId = interaction.options.getUser('target-user')?.id || interaction.user.id;
+
+    await interaction.deferReply();
+
+    try {
+      let userProfile = await UserProfile.findOne({ userId: targetUserId });
 
       if (!userProfile) {
-        userProfile = new UserProfile({
-          userId: interaction.member.id,
-          balance: 0
-        });
-
-        await userProfile.save();
+        userProfile = new UserProfile({ userId: targetUserId });
       }
 
       interaction.editReply(
-        `**Current balance: ${userProfile.balance} $PTC**`
+        targetUserId === interaction.user.id ? `**Your balance is ${userProfile.balance} $PTC**` : `<@${targetUserId}>\nBalance: **${userProfile.balance} $PTC**`
       );
     } catch (error) {
       console.log(`Error handling /balance: ${error}`);
